5adaf11f32c02a7d90d008caea01f8227a0cd6c6
janvn <wpzhan1989@163.com>
2019-08-06 22:55:45 +0800

    Initial commit

---
+++ chapter03/index.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+
+import sys
+import re
+
+WORD_RE = re.compile(r'\w+')
+
+#sys.argv[1] = './data/zen.txt'
+
+index = {}
+with open('./data/zen.txt', encoding='utf-8') as fp:
+    for line_no, line in enumerate(fp, 1):
+        for match in WORD_RE.finditer(line):
+            word = match.group()
+            column_no = match.start() + 1
+            location = (line_no, column_no)
+            index.setdefault(word,[]).append(location)
+
+for word in sorted(index, key=str.upper):
+    print(word, index[word])
